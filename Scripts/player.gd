046729989c536d7fe6ad46f08c4d99e8809625d1
Qiru1 <shiera1@proton.me>
2024-11-20 15:21:08 +0200

    direction change working

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -19,8 +19,9 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.flip_h = (direction == -1)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	$AnimatedSprite2D.flip_h = direction
+		
 
 	move_and_slide()
