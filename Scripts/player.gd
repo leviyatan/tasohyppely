256b22ac3eefe4307dd187a316d997ba868997ac
leviyatan <esa.jarvanne@gmail.com>
2024-11-27 18:22:57 +0200

    bullet work in progress

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,17 @@
 extends CharacterBody2D
 
+const bulletPath = preload('res://Scenes/bullet.tscn')
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -300.0
 
 var double_jump = 1
+
+func shoot():
+	var bullet = bulletPath.instantiate()
+	
+	get_parent().add_child(bullet)
+	bullet.position = $Marker2D.global_position
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -19,6 +26,9 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 		if not is_on_floor():
 			double_jump = double_jump - 1
+	
+	if Input.is_action_just_pressed("ui_down"):
+		shoot()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
