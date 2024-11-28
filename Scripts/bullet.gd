c381d769034cc503adb45f154bb006f7f648aaea
leviyatan <esa.jarvanne@gmail.com>
2024-11-28 09:53:29 +0200

    bullets can disappear

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,9 +1,15 @@
 extends CharacterBody2D
 
+const hostile_script = preload('res://Scenes/world.tscn')
 
 var bullet_velocity = Vector2(1, 0)
 var bullet_speed = 300
 
-
 func _physics_process(delta: float) -> void:
-	var collision_info = move_and_collide(bullet_velocity.normalized() * delta * bullet_speed)
+	var collision = move_and_collide(bullet_velocity.normalized() * delta * bullet_speed)
+	if collision:
+		if collision.get_collider().name == "hostile1":
+			pass
+			#$hostile1.dead = true
+		print(collision.get_collider().name)
+		queue_free()
